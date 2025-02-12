bbe56c78f5e162de6fe91df08a9adbe1ae5fdbc4
Lucas Machado <lucas.machado.professional@gmail.com>
2025-02-12 11:22:25 -0500

    Setup basic server, able to read from the .env file

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/LucasAMachado/rss-aggregator
+
+go 1.23.4
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -2,9 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	
-	fmt.Println("Hello World")
+	// Allow us to read the .env file
+	godotenv.Load(".env")
+	
+	portString := os.Getenv("PORT")
+	
+	// break out of the program with error message (could not find port in our .env file)
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server {
+		Handler : router,
+		Addr : ":" + portString,
+	}
+
+	log.Printf("Server is starting on port %v", portString)
+	err := srv.ListenAndServe() // the code should stop here and we should handle all the http requests etc
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	fmt.Println("PORT:", portString)
+
+
 }
